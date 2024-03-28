87ec8fc7710c0a88fa1280ac3d34db05d016afd3
Chimy-Cent <chimyinnocent@gmail.com>
2024-03-28 20:57:09 +0100

    first commit

---
+++ src/components/TeamMessage.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import { MessageTeam, useMessageContext } from 'stream-chat-react';
+
+// const TeamMessage = () => {
+//     const { handleOpenThread, message } = useMessageContext();
+
+//     return (
+//         // <MessageTeam
+//         //     message={{ ...message, user: {}}}
+//         //     // handleOpenThread={}
+//         // />
+//     )
+// }
+
+// export default TeamMessage
+
+
+import React from 'react'
+
+const TeamMessage = () => {
+  return (
+    <div>TeamMessage</div>
+  )
+}
+
+export default TeamMessage
